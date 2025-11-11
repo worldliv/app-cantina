fd4f3d44c047101ecd4eba817b1ef9bdbcf9bb66
Valenthini <valenthini.e@gmail.com>
2025-11-11 16:48:56 -0300

    Update HomePage

--- screens/home.js
+++ screens/home.js
@@ -1,2 +1,34 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
+
+//Aqui iniciaremos criando nossos cards, onde estará sendo exibida nossa alimentação.
+//Através dos Cards, informações estarão contidas ao clicar no mesmo.
+
+function Card() {
+    return (
+//Nessa espécie de "DIV" estaremos adicionando os componentes de nossa Página Inicial.
+//"Card" será o espaço reservado para a amostra de produtos (alimentos).
+//"cardTitle" e "cardContent"
+        <View style={styles.card}>
+            <Text style={styles.cardTitle}>Título Card</Text>
+            <Text style={styles.cardContent}>Conteúdo Card</Text>
+        </View>
+    ) 
+}
+//A partir daqui, se inicia a estilização de nossos componentes.
+StyleSheet.create({
+    card: {
+        backgroundColor: 'purple',
+        borderRadius: 4,
+        padding: 12,
+        //Margem no eixo Y
+        marginVertical: 8,
+        //Sombreamento no *Android
+        elevation:5,
+
+    },
+    cardTitle: {
+    },
+    cardContent: {
+    },
+})
